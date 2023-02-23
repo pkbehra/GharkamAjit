b0964f680aa0649c1487da74872378a3712a9243
pritesh <centriotech@gmail.com>
2023-02-23 12:38:19 +0530

    Pritesh Commit

---
+++ app/src/main/java/com/example/gharkam/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.gharkam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Userdashboard::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
